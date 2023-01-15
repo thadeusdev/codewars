f979281747c3a6e3a69a04a776d31836875634f5
thadeusdev <109853680+thadeusdev@users.noreply.github.com>
2023-01-16 01:09:08 +0300

    Update mean-square-error.js

--- mean-square-error.js
+++ mean-square-error.js
@@ -8,3 +8,7 @@ var solution = function(firstArray, secondArray) {
     }
 }
 console.log(solution([10, 20, 10, 2], [10, 25, 5, -2]))
+
+// [1, 2, 3], [4, 5, 6]              -->   9   because (9 + 9 + 9) / 3
+// [10, 20, 10, 2], [10, 25, 5, -2]  -->  16.5 because (0 + 25 + 25 + 16) / 4
+// [-1, 0], [0, -1]                  -->   1   because (1 + 1) / 2
